3e4faa57e96011d6a7bdb9f4fc03b0a4087d9315
Arthur de Araujo Custódio <141514740+ArthurDev-A@users.noreply.github.com>
2026-04-23 15:36:30 -0300

    bees 1174 e 1176

---
+++ Tarefa de Laboratório 03/bee-1176-Fibonacci_em_Vetor.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+int main() {
+    int t, n, aux=2;// começa em 2 pois as posições 0 e 1 são definidas no início
+    unsigned long long v[61];
+    v[0] = 0;
+    v[1] = 1;
+
+    scanf("%d", &t);
+
+    for (int i=0; i<t; i++) {
+        scanf("%d", &n);
+
+        if (aux<=n) {//verifica se já foi calculado o fibonacci
+            for (int j=aux; j<=n; j++) //calcula fibonacci
+                v[j] = v[j-1] + v[j-2];
+            aux = n+1;
+        }
+
+        printf("Fib(%d) = %llu\n", n, v[n]);
+    }
+
+    return 0;
+}
