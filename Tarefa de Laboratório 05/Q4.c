ad2f7af4af0aa1f3faade1ba0903dd96ff94637b
Arthur de Araujo Custódio <141514740+ArthurDev-A@users.noreply.github.com>
2026-05-14 21:41:37 -0300

    Add files via upload

--- Tarefa de Laboratório 05/Q4.c
+++ Tarefa de Laboratório 05/Q4.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
 
+int main() {
+    unsigned int n, sum=0, i=1, i_max;
+    scanf("%u", &n);
+    i_max = n/2;
+
+    while((i<=i_max) && (sum <= n)) {
+        if (n%i == 0)
+            sum+=i;
+        i++;
+    }
+
+    printf((sum == n) ? "Eh perfeito" : "Nao eh perfeito");
+
+    return 0;
+}
