6a0770366063f2677ce8276f4e7972dbaa570a43
Arthur de Araujo Custódio <141514740+ArthurDev-A@users.noreply.github.com>
2026-05-14 22:38:49 -0300

    Add files via upload

--- Tarefa de Laboratório 05/Q6.c
+++ Tarefa de Laboratório 05/Q6.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+double pot2(int n) {
+    if (n<0) return 1./pot2(-n);
+    if (n == 0) return 1.;
+    return (double) 2.*pot2(n-1);
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    printf("%f\n", pot2(n));
+
+    return 0;
+}
