2e7d821be5b5fdbdf47f361d1626c33934c2ade1
Arthur de Araujo Custódio <141514740+ArthurDev-A@users.noreply.github.com>
2026-05-14 22:05:08 -0300

    Add files via upload

--- Tarefa de Laboratório 05/Q5.c
+++ Tarefa de Laboratório 05/Q5.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
 
+long int inverte(int n) {
+    int p10 = 1, aux = n;
+    if (n<0) return -inverte(n);
+    if (n<10) return n;
+
+    while(aux>10) {
+        p10*=10;
+        aux/=10;
+    }
+
+    return (n%10) * p10 + inverte(n/10);
+
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    printf("%ld", inverte(n));
+
+    return 0;
+}
