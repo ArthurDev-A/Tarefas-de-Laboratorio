9cfaa8fe51c0906a889755576fa2801b1964dc20
Arthur de Araujo Custódio <141514740+ArthurDev-A@users.noreply.github.com>
2026-05-17 13:34:05 -0300

    Fix percentage output format in Experiencias.c

--- Tarefa de Laboratório 01/bee-1094-Experiencias.c
+++ Tarefa de Laboratório 01/bee-1094-Experiencias.c
@@ -1,15 +1,13 @@
-// https://judge.beecrowd.com/pt/problems/view/1094
-
 #include <stdio.h>
 
 int main()
 {
-    int n, qtd=0, qtdTotal=0, qtdC=0, qtdR=0, qtdS=0;
+    int n, i, qtd=0, qtdTotal=0, qtdC=0, qtdR=0, qtdS=0;
     char tipo;
     
     scanf("%d", &n);
     
-    for (int i=0; i<n; i++) {
+    for (i=0; i<n; i++) {
         scanf("%d %c", &qtd, &tipo);
         qtdTotal += qtd;
         
@@ -17,15 +15,12 @@ int main()
             case 'C':
                 qtdC+=qtd;
                 break;
-                
             case 'R':
                 qtdR+=qtd;
                 break;
-                
             case 'S':
                 qtdS+=qtd;
                 break;
-                
         }
     }
     
@@ -33,9 +28,9 @@ int main()
     printf("Total de coelhos: %d\n", qtdC);
     printf("Total de ratos: %d\n", qtdR);
     printf("Total de sapos: %d\n", qtdS);
-    printf("Percentual de coelhos: %.2f %\n", (float) qtdC/qtdTotal*100);
-    printf("Percentual de ratos: %.2f %\n", (float) qtdR/qtdTotal*100);
-    printf("Percentual de sapos: %.2f %\n", (float) qtdS/qtdTotal*100);
+    printf("Percentual de coelhos: %.2f %%\n", (float) qtdC/qtdTotal*100);
+    printf("Percentual de ratos: %.2f %%\n", (float) qtdR/qtdTotal*100);
+    printf("Percentual de sapos: %.2f %%\n", (float) qtdS/qtdTotal*100);
 
     return 0;
 }
