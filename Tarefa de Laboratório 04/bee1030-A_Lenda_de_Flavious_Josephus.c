543544b813fe2c1d934d30e39494ae6c6d8f30b4
Arthur de Araujo Custódio <141514740+ArthurDev-A@users.noreply.github.com>
2026-05-17 12:23:34 -0300

    Format code and fix indentation in Josephus problem

--- Tarefa de Laboratório 04/bee1030-A_Lenda_de_Flavious_Josephus.c
+++ Tarefa de Laboratório 04/bee1030-A_Lenda_de_Flavious_Josephus.c
@@ -9,7 +9,7 @@ int main() {
     int nc, i, n, k;
     scanf("%d", &nc);
 
-    for (i=1; i<=nc; i++){
+    for (i=1; i<=nc; i++) {
         scanf("%d %d", &n, &k); 
         printf("Case %d: %d\n", i, LendaFlaJo(n, k)+1);
     }
