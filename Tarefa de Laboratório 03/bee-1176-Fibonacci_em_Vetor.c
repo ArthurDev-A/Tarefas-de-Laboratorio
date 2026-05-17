28dc5ff632f27eaa7ca20e91687f60ee0da7e66e
Arthur de Araujo Custódio <141514740+ArthurDev-A@users.noreply.github.com>
2026-05-17 13:04:42 -0300

    Fix formatting and comments in Fibonacci program

--- Tarefa de Laboratório 03/bee-1176-Fibonacci_em_Vetor.c
+++ Tarefa de Laboratório 03/bee-1176-Fibonacci_em_Vetor.c
@@ -2,7 +2,7 @@
 
 
 int main() {
-    int t, n, i, j, aux=2;/* começa em 2 pois as posições 0 e 1 são definidas no início */
+    int t, n, i, j, aux=2; /* começa em 2 pois as posições 0 e 1 são definidas no início */
     double v[61]; /* double, pois em C ANSI nao tem unsigned long long int */
     v[0] = 0;
     v[1] = 1;
@@ -12,7 +12,7 @@ int main() {
     for (i=0; i<t; i++) {
         scanf("%d", &n);
 
-        if (aux<=n) {/* verifica se já foi calculado o fibonacci */
+        if (aux<=n) { /* verifica se já foi calculado o fibonacci */
             for (j=aux; j<=n; j++) /* calcula fibonacci */
                 v[j] = v[j-1] + v[j-2];
             aux = n+1;
